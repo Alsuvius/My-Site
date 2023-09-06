288d86db4c0c58aabf2809fe315b22be891cd855
Alex Price <a-r-price@hotmail.co.uk>
2023-09-06 16:52:27 +0100

    reduced asset file sizes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+window.onload = function() {
     var topBanners = document.querySelectorAll('.top-banner');
     var header1 = document.querySelectorAll('h1');
     var header2 = document.querySelectorAll('h2');
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
     topBanners.forEach(function(topBanner) {
       setTimeout(function() {
         topBanner.style.minHeight = '40vh';
-      }, 4000); // Add a 3-second delay to change min-height
+      }, 4000); 
     });
     header1.forEach(function(header1) {
         setTimeout(function() {
           header1.style.opacity = '90%';
-        }, 1000); // Add a 3-second delay to change min-height
+        }, 1000); 
       });
     header2.forEach(function(header2) {
         setTimeout(function() {
             header2.style.opacity = '90%';
-        }, 2000); // Add a 3-second delay to change min-height
+        }, 2000);
       });
-  });
+  };
   
